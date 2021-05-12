1e8a8f473d0f4e1f9d343269268c29f66a5a760b
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-05-12 16:24:49 -0700

    up

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require('express');
-const app = express;
+const app = express();
+const port = 8000;
+
+// app.get('/', (req,res) => {
+//     res.status(200).send('hello from server');
+// });
+
+app.get('/', (req,res) => {
+    res.status(200).json({'message':'hello from server', 'name':'isaac ameh'});
+});
+
+
+
+app.listen(port, ()=>{
+    console.log(`sever started at ${port}`);
+
+});
