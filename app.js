5e8a38f94c2da66ba6d6571e472729d5807c3a4e
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-05-12 18:24:05 -0700

    test

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 8000;
 
+const tours = fs.readFileSync(`${__dirname}/dev-date/tours-simple.json`)
 // app.get('/', (req,res) => {
 //     res.status(200).send('hello from server');
 // });
 
-app.get('/', (req,res) => {
-    res.status(200).json({'message':'hello from server', 'name':'isaac ameh'});
-});
+// app.get('/', (req,res) => {
+//     res.status(200).json({'message':'hello from server', 'name':'isaac ameh'});
+// });
+
+
+app.get('api/v1/tours',(req, res)=>{
+   res.status(200).json({ 
+       status:'success',
+       data:{
+           tours
+       }
+    })
+})
 
 
 
