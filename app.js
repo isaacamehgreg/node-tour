5d943c1b2a87af2e673a626402baba4cd775ab3a
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-05-14 01:22:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const fs = require('fs');
 const port = 8000;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
 app.use(express.json());//this is a middleware
-// app.get('/', (req,res) => {
-//     res.status(200).send('hello from server');
-// });
+
+app.get('/', (req,res) => {
+    console.log('i was checked')
+    res.status(200).send('hello from server');
+});
 
 // app.get('/', (req,res) => {
 //     res.status(200).json({'message':'hello from server', 'name':'isaac ameh'});
 // });
 
-
+//get tours
 app.get('/api/v1/tours',(req, res)=>{
    res.status(200).json({ 
        status:'success',
@@ -25,10 +28,38 @@ app.get('/api/v1/tours',(req, res)=>{
     
 })
 
+
+//create a tour
 app.post('/api/v1/tours',(req,res)=>{
-    cosole.log(req.body);
-    res.send('good')
-})
+    
+    console.log(req.body);
+    res.send(`good`)
+});
+
+
+//get a single tour
+
+app.get('/api/v1/tours/:id/:x?/:y?', (req,res)=>{
+
+
+    const id = req.params.id *1;
+
+    const tour = tours.find(el =>el.id === req.params.id);
+    res.status(200).json({ 
+        status:'success',
+       
+        data:{
+            tour,
+        }
+     }
+     );
+
+
+     console.log(req.params);
+     res.send('success');
+});
+
+
 
 app.listen(port, ()=>{
     console.log(`sever started at ${port}`);
